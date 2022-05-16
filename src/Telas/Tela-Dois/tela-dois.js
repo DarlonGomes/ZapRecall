f754382ef3bc36c1821410966de41ad48b95cbeb
Darlon Gomes <gomesdarlon@icloud.com>
2022-05-16 00:37:09 -0300

    Resultado no Footer

--- src/Telas/Tela-Dois/tela-dois.js
+++ src/Telas/Tela-Dois/tela-dois.js
@@ -5,7 +5,7 @@ export default function TelaDois (){
     function comparador() { 
         return Math.random() - 0.5; 
     }
-
+    
     const perguntas = [
         {topico: "Pergunta",
          pergunta: "Q: O que é JSX?",
@@ -40,9 +40,9 @@ export default function TelaDois (){
          resposta: "R: dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
         },
     ]
-
     perguntas.sort(comparador);
-    let contador = 0;
+    const [resultado,setResultado] = React.useState([]);
+    const [erro, setErro] = React.useState(0);
 
     return(
         <>
@@ -51,55 +51,61 @@ export default function TelaDois (){
             <h1>ZapRecall</h1>
         </header>
         <main>
-            {perguntas.map((item, index)=> <Cartas key={index} index={index + 1} topico={item.topico} pergunta={item.pergunta} resposta={item.resposta} contador={contador}/>)}
+            {perguntas.map((item, index)=> <Cartas key={index} index={index + 1}  pergunta={item.pergunta} resposta={item.resposta} 
+            resultado={resultado} setResultado={setResultado} erro={erro} setErro={setErro}/>)}
+            <div class="espaçador"></div>
         </main>
         <footer>
-            <p>{contador}/{perguntas.length} CONCLUÍDOS</p>
+            <Resultado resultado={resultado} perguntas={perguntas} erro={erro}/>
         </footer>
         </>
     )
 }
 
 function Cartas (props){
-    const {topico, pergunta, resposta, index, contador} = props;
+    const {pergunta, resposta, index, resultado, setResultado, erro, setErro} = props;
     const [flashTexto, setFlashTexto] = React.useState(`Pergunta ${index}`);
     const [flashClasse, setFlashClasse] = React.useState(`topico`);
-    const [flashBotao, setFlashBotao] = React.useState(`play`)
-    const [flashResposta, setFlashResposta] = React.useState(``)
-    const [flashContador, setFlashContador] = React.useState(`0`)
+    const [flashBotao, setFlashBotao] = React.useState(`play`);
+    const [flashResposta, setFlashResposta] = React.useState(``);
+    
+
     function flashClick (indicador){
         if(flashBotao === `play`){
             setFlashTexto(pergunta);
             setFlashClasse(`questao`);
             setFlashBotao(`viraCarta`);
-            console.log(contador)
         }
         if(flashBotao === `viraCarta`){
+            setFlashClasse(`questao costas resposta`);
             setFlashTexto(resposta);
-            setFlashBotao(`opcoes`)
+            setFlashBotao(`opcoes`);
         }
         if(flashBotao === `opcoes` && indicador === `errado`){
             setFlashTexto(`Pergunta ${index}`);
             setFlashClasse(`topico respondido errado`);
-            setFlashResposta (`errado`)
-            setFlashBotao(`played`)
-            
+            setFlashResposta (`errado`);
+            setFlashBotao(`played`);
+            const icone = [...resultado, {item: <ion-icon name="close-circle"></ion-icon>}]
+            setResultado(icone);
+            setErro(erro + 1);
         }
         if(flashBotao === `opcoes` && indicador === `quase`){
             setFlashTexto(`Pergunta ${index}`);
             setFlashClasse(`topico respondido quase`);
-            setFlashResposta (`quase`)
-            setFlashBotao(`played`)
-            
+            setFlashResposta (`quase`);
+            setFlashBotao(`played`);
+            const icone = [...resultado, {item: <ion-icon name="help-circle"></ion-icon>}];
+            setResultado(icone);
         }
         if(flashBotao === `opcoes` && indicador === `certo`){
             setFlashTexto(`Pergunta ${index}`);
             setFlashClasse(`topico respondido certo`);
-            setFlashResposta (`certo`)
-            setFlashBotao(`played`)
-            
+            setFlashResposta (`certo`);
+            setFlashBotao(`played`);
+            const icone =[...resultado, {item: <ion-icon name="checkmark-circle"></ion-icon>}];
+            setResultado(icone);
         }
-
     }
     return (
     <>
@@ -110,4 +116,47 @@ function Cartas (props){
     </>
     )
 }   
+
+function Resultado (props) {
+    const {resultado, perguntas, erro} = props;
+
+    if(resultado.length !== 0 && resultado.length < 8){
+        return(
+        <>
+        <p>{resultado.length}/{perguntas.length} CONCLUÍDOS</p>
+        <div className="icones">{resultado.map(icone=> icone.item)}</div>
+        </>
+
+        )
+    }
+
+    if(resultado.length === 8 && erro !== 0){
+        return (
+            <>
+            <h4><img src="./assets/sad.png" alt="Carinha triste"/>Puts...</h4>
+            <p> Ainda faltam alguns... <br />
+                Mas não desanime!</p>
+                <p>{resultado.length}/{perguntas.length} CONCLUÍDOS</p>
+            <div className="icones">{resultado.map(icone=> icone.item)}</div>
+            </>
+        )
+    }
+
+    if(resultado.length === 8   ){
+        return (
+            <>
+            <h4><img src="./assets/party.png" alt="Festa"/>Parabéns!</h4>
+            <p> Você não esqueceu de <br/>
+                nenhum flashcard!</p>
+                <p>{resultado.length}/{perguntas.length} CONCLUÍDOS</p>
+            <div className="icones">{resultado.map(icone=> icone.item)}</div>
+            </>
+        )
+    }
+    return(
+        <>
+        <p>{resultado.length}/{perguntas.length} CONCLUÍDOS</p>
+        </>
+    )
+}
 
