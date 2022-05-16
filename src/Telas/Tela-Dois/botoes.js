a4c22d0aefa26299cc2f8d1e7a21d3c5362c6db2
Darlon Gomes <gomesdarlon@icloud.com>
2022-05-15 21:41:37 -0300

    Implementação dos botões de ZAP

--- src/Telas/Tela-Dois/botoes.js
+++ src/Telas/Tela-Dois/botoes.js
@@ -9,7 +9,20 @@ export default function Botoes ({flashBotao, flashResposta, flashClick}){
     if(flashBotao === `viraCarta`){
         return(
             <>
-            <img src="./assets/setinha.png" />
+            <div class="secao-pergunta">
+            <img onClick={flashClick} src="./assets/setinha.png" />
+            </div>
+            </>
+        )
+    }
+    if(flashBotao === `opcoes`){
+        return(
+            <>
+            <div class="secao-botoes">
+            <button className="backErrado"><p>Não lembrei</p> </button>
+            <button className="backQuase"><p> Quase não lembrei</p> </button>
+            <button className="backZap"><p>Zap!</p> </button>
+            </div>
             </>
         )
     }
